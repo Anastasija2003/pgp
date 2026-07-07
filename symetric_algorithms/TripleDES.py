564dfcde9e5bf917a8ab3bc7a8aedf0328a4396a
Anastasija2003 <milanovictasa8@gmail.com>
2026-07-07 20:55:59 +0200

    Added symetric algorithms

--- symetric_algorithms/TripleDES.py
+++ symetric_algorithms/TripleDES.py
@@ -1,20 +1,21 @@
 import os
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.ciphers import Cipher, modes
+from cryptography.hazmat.decrepit.ciphers.algorithms import TripleDES as _TripleDESAlgorithm
 
 from symetric_algorithms.SymetricAlgorithm import SymetricAlgorithm
 
 
-class AES128(SymetricAlgorithm):
-    IV_SIZE = 16
+class TripleDES(SymetricAlgorithm):
+    IV_SIZE = 8
 
     def encrypt(self, input: bytes) -> bytes:
         iv = os.urandom(self.IV_SIZE)
-        encryptor = Cipher(algorithms.AES128(self.key), modes.CFB(iv)).encryptor()
+        encryptor = Cipher(_TripleDESAlgorithm(self.key), modes.CFB(iv)).encryptor()
         ciphertext = encryptor.update(input)
         return iv + ciphertext
 
     def decrypt(self, input: bytes) -> bytes:
         iv = input[:self.IV_SIZE]
         ciphertext = input[self.IV_SIZE:]
-        decryptor = Cipher(algorithms.AES128(self.key), modes.CFB(iv)).decryptor()
+        decryptor = Cipher(_TripleDESAlgorithm(self.key), modes.CFB(iv)).decryptor()
         return decryptor.update(ciphertext)
